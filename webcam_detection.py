9091783bab6fdf061c92ca2e17d25e7b902a4934
Rishikathegenius <75022017+Rishikathegenius@users.noreply.github.com>
2021-06-04 16:50:35 +0530

    Update webcam_detection.py

--- webcam_detection.py
+++ webcam_detection.py
@@ -288,67 +288,67 @@ run = st.checkbox("Run", key='checkbox')
 # FRAME_WINDOW = st.image([])
 
 
-# class VideoTransformer(VideoTransformerBase):
-#     def transform(self, frame):
-with detection_graph.as_default():
-    with tf.Session(graph=detection_graph) as sess:
-#                 image_np = frame.to_ndarray(format="bgr24")
-        _, image_np = cap.read()
-        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image_np, axis=0)
-        image_tensor = detection_graph.get_tensor_by_name(
-            "image_tensor:0")
-        # Each box represents a part of the image where a particular object was detected.
-        boxes = detection_graph.get_tensor_by_name("detection_boxes:0")
-        # Each score represent how level of confidence for each of the objects.
-        # Score is shown on the result image, together with the class label.
-        scores = detection_graph.get_tensor_by_name(
-            "detection_scores:0")
-        classes = detection_graph.get_tensor_by_name(
-            "detection_classes:0")
-        num_detections = detection_graph.get_tensor_by_name(
-            "num_detections:0")
-        # Actual detection.
-        (boxes, scores, classes, num_detections) = sess.run(
-            [boxes, scores, classes, num_detections],
-            feed_dict={image_tensor: image_np_expanded},
-        )
-        # Visualization of the results of a detection.
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            image_np,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=3,
-        )
-        my_list = [
-            category_index.get(value)
-            for index, value in enumerate(classes[0])
-            if scores[0, index] > 0.5
-        ]
-        res = {}
-        for line in my_list:
-            res.update(line)
-            # print("Res", res)
-            t2s("I can see a" + res["name"] + "is here")
+class VideoTransformer(VideoTransformerBase):
+     def transform(self, frame):
+        with detection_graph.as_default():
+            with tf.Session(graph=detection_graph) as sess:
+        #                 image_np = frame.to_ndarray(format="bgr24")
+                _, image_np = cap.read()
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                image_tensor = detection_graph.get_tensor_by_name(
+                    "image_tensor:0")
+                # Each box represents a part of the image where a particular object was detected.
+                boxes = detection_graph.get_tensor_by_name("detection_boxes:0")
+                # Each score represent how level of confidence for each of the objects.
+                # Score is shown on the result image, together with the class label.
+                scores = detection_graph.get_tensor_by_name(
+                    "detection_scores:0")
+                classes = detection_graph.get_tensor_by_name(
+                    "detection_classes:0")
+                num_detections = detection_graph.get_tensor_by_name(
+                    "num_detections:0")
+                # Actual detection.
+                (boxes, scores, classes, num_detections) = sess.run(
+                    [boxes, scores, classes, num_detections],
+                    feed_dict={image_tensor: image_np_expanded},
+                )
+                # Visualization of the results of a detection.
+                vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    category_index,
+                    use_normalized_coordinates=True,
+                    line_thickness=3,
+                )
+                my_list = [
+                    category_index.get(value)
+                    for index, value in enumerate(classes[0])
+                    if scores[0, index] > 0.5
+                ]
+                res = {}
+                for line in my_list:
+                    res.update(line)
+                    # print("Res", res)
+                    t2s("I can see a" + res["name"] + "is here")
 
-        # camera = cv2.VideoCapture(0)
+                # camera = cv2.VideoCapture(0)
 
-        # while run:
-        # _, frame = camera.read()
-        # frame = cv2.imshow("object detection",
-        #                    cv2.resize(image_np, (800, 600)))
-        # dataBytesIO = io.BytesIO(frame)
-        FRAME_WINDOW.image(image_np)
-        # else:
-        #     st.write("Stopped")
+                # while run:
+                # _, frame = camera.read()
+                # frame = cv2.imshow("object detection",
+                #                    cv2.resize(image_np, (800, 600)))
+                # dataBytesIO = io.BytesIO(frame)
+                FRAME_WINDOW.image(image_np)
+                # else:
+                #     st.write("Stopped")
 
-        # if cv2.waitKey(25) & 0xFF == ord('q'):
-        #     cv2.destroyAllWindows()
-        #     break
-#         return image_np
+                # if cv2.waitKey(25) & 0xFF == ord('q'):
+                #     cv2.destroyAllWindows()
+                #     break
+                 return image_np
 
 
-# webrtc_streamer(key="helloworld", video_transformer_factory=VideoTransformer)
+         webrtc_streamer(key="helloworld", video_transformer_factory=VideoTransformer)
